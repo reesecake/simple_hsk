ea69b1ce2e3eeeafcf49c33fed61f4d8a8f25ea1
Nathan Mohapatra <nathan.mohapatra@gmail.com>
2021-08-09 19:04:20 -0700

    Created and linked HSK HTML files

--- src/main/java/com/reesecake/simple_hsk/HomeController.java
+++ src/main/java/com/reesecake/simple_hsk/HomeController.java
@@ -1,7 +1,10 @@
 package com.reesecake.simple_hsk;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -15,4 +18,12 @@ public class HomeController {
     public String about() {
         return "about";
     }
+
+    @RequestMapping(value = "/hsk/{level_id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String hsk(
+        @PathVariable("level_id") long level_id) {
+            return "hsk" + level_id;
+    }
+
 }
