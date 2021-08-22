44b1549c4d51512f073c108d4e3f301f5c5c4343
Reese <rlam2017@gmail.com>
2021-08-22 02:26:26 -0700

    removed Employee class, repo, tests

--- src/main/java/com/reesecake/simple_hsk/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.reesecake.simple_hsk;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
-}
