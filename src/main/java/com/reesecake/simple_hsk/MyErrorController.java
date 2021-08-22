de1439a559156dd61f8ba5ffd8a465c8dc8aaff7
Nathan Mohapatra <nathan.mohapatra@gmail.com>
2021-08-21 23:56:09 -0700

    Account for more HTTP status codes

--- src/main/java/com/reesecake/simple_hsk/MyErrorController.java
+++ src/main/java/com/reesecake/simple_hsk/MyErrorController.java
@@ -18,10 +18,36 @@ public class MyErrorController implements ErrorController {
         if (status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
 
-            if(statusCode == HttpStatus.BAD_REQUEST.value()) {
+            if (statusCode == HttpStatus.BAD_REQUEST.value()) {
                 return "error/400";
             }
-            // TODO: Map to HTML pages for other error statuses
+            else if (statusCode == HttpStatus.UNAUTHORIZED.value()) {
+                return "error/401";
+            }
+            else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+                return "error/403";
+            }
+            else if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error/404";
+            }
+            else if (statusCode == HttpStatus.METHOD_NOT_ALLOWED.value()) {
+                return "error/405";
+            }
+            else if (statusCode == HttpStatus.NOT_ACCEPTABLE.value()) {
+                return "error/406";
+            }
+            else if (statusCode == HttpStatus.PRECONDITION_FAILED.value()) {
+                return "error/412";
+            }
+            else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error/500";
+            }
+            else if (statusCode == HttpStatus.NOT_IMPLEMENTED.value()) {
+                return "error/501";
+            }
+            else if (statusCode == HttpStatus.BAD_GATEWAY.value()) {
+                return "error/502";
+            }
 
         }
         return "error";
