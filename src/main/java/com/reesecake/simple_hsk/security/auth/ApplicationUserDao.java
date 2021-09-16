6bcea2ba08667602ec7c29e89fd7807b1a44533b
Nathan Mohapatra <nathan.mohapatra@gmail.com>
2021-09-15 23:18:36 -0700

    Database authentication setup

---
+++ src/main/java/com/reesecake/simple_hsk/security/auth/ApplicationUserDao.java
@@ -0,0 +1,9 @@
+package com.reesecake.simple_hsk.security.auth;
+
+import java.util.Optional;
+
+public interface ApplicationUserDao {
+
+    Optional<ApplicationUser> selectApplicationUserByUsername(String username);
+
+}
